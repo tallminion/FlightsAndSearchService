31d13734ffccdda1413022a52ba38f4461f2f40f
tallminion <sagarkr1109@gmail.com>
2024-08-21 00:49:37 +0530

    to synchronize the db

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const { PORT } = require("./config/serverConfig");
 const bodyParser = require("body-parser");
 const ApiRoutes = require('./routes/index');
 
+const {Airport, City} = require('./models/index');
+const { sequelize } = require("sequelize");
+
+const db = require('./models/index');
+
 const setupAndStartServer = async () => {
     const app = express();
 
@@ -10,8 +15,45 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use('/api', ApiRoutes);
     
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server started at ${PORT}`);
+
+        const airports = await City.findAll({
+            where: {
+                id: 2
+            },
+            include: [
+                {
+                    model: Airport
+                }
+            ]
+        });
+        // console.log(JSON.stringify(airports, null, 2));
+
+        db.sequelize.sync({alter: true});
+
+        // const city = await City.findOne({
+        //     where:{
+        //         id: 2
+        //     }
+        // });
+        // const airports = await city.getAirports();
+        // const newAirport = await Airport.findAll({
+        //     // name: "Jindal Vijayanagar Airport",
+        //     // cityId: 3
+        //     where: {
+        //         name: "Jindal Vijayanagar Airport"
+        //     }
+        // });
+
+        // await city.addAirport(newAirport);
+
+        // // await city.addAirport({
+        // //     name: 'Jindal Vijaynagar Airport'
+        // // })
+        
+        // console.log(city, airports);
+
     });
 }
 
