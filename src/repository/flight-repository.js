1d252960436507e2181cc5cf35c46664c4e6a799
tallminion <sagarkr1109@gmail.com>
2024-09-14 16:11:02 +0530

    updated flights search custom filters

--- src/repository/flight-repository.js
+++ src/repository/flight-repository.js
@@ -4,13 +4,15 @@ const { Flights } = require('../models/index');
 class FlightRepository{
 
     #createFilter(data){
-        let filter = {};
-        if(data.arrivalAirportId){
-            filter.arrivalAirportId = data.arrivalAirportId;
-        }
-        if(data.departureAirportId){
-            filter.departureAirportId = data.departureAirportId;
-        }
+        let filter = {...data};
+        // if(data.arrivalAirportId){
+        //     filter.arrivalAirportId = data.arrivalAirportId;
+        // }
+        // if(data.departureAirportId){
+        //     filter.departureAirportId = data.departureAirportId;
+        // }
+        // above if's can be avoided by using destructiuring of data by ...data in filter
+        
         // if(data.minPrice){
         //     Object.assign(filter, {price: {[Op.gte]: data.minPrice}});
         // }
